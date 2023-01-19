543a11e8c4b9fb0492a498aca3aff228b8ce59b1
Khyber Sen <kkysen@gmail.com>
2023-01-19 17:02:43 -0500

    Added the basic CLI with `clap` and `humantime` and `dateparser` for date time parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+use dateparser::DateTimeUtc;
+use humantime::Duration;
+use std::str::FromStr;
+
+#[derive(Clone, Debug)]
+enum DateTime {
+    DateTime(DateTimeUtc),
+    Ago(Duration),
+}
+
+impl FromStr for DateTime {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.strip_suffix(" ago") {
+            Some(ago) => Self::Ago(Duration::from_str(ago)?),
+            None => Self::DateTime(DateTimeUtc::from_str(s)?),
+        })
+    }
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(long)]
+    repo: String,
+
+    #[clap(long)]
+    after: Option<DateTime>,
+
+    #[clap(long)]
+    before: Option<DateTime>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(&args);
 }
